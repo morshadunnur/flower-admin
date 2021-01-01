3c0e602bfc729736d6107d990deaef50da21638b
morshadunnur <13203016@iubat.edu>
2021-01-02 00:05:56 +0600

    order migration added

---
+++ database/migrations/2021_01_01_172609_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->tinyInteger('global_order_status')->default(1)->comment('
+                1 = placed
+                2 = processing
+                3 = Ready to delivery
+                4 = Delivered to courier
+                5 = Deliver completed
+            ');
+            $table->enum('type', ['cart','wishlist','order','later','offer']);
+            $table->text('address')->nullable();
+            $table->text('shipping_address')->nullable();
+            $table->string('remarks')->nullable();
+            $table->dateTime('placed_date')->nullable();
+            $table->boolean('mail_sent')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
